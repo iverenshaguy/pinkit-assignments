a2bc515b39e7835b2e4fb653be20914c24665cef
Iveren Favour Shaguy <iverenshaguy@gmail.com>
2017-03-28 13:40:41 +0100

    Function to convert numbers to binary.
    I used the decbin() function.

---
+++ assign1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Convert Numbers to Binary</title>
+</head>
+
+<body>
+
+<?php
+//We simply use the decbin() function.
+
+function dectobin($num) {
+  echo decbin($num);
+}
+
+dectobin(1673);
+
+?>
+
+</body>
+
+
+</html>
