20196d70ca0921e8000dc860f104737ef5461856
Iveren Favour Shaguy <iverenshaguy@gmail.com>
2017-03-28 13:48:26 +0100

    Form input that prints details on the same page.

---
+++ assign4.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Form</title>
+    <style>
+        form {
+            width: 300px;
+            margin: auto;
+            padding-top: 100px;
+        }
+        
+        input {
+            padding: 8px 3px;
+            width: 100%;
+        }
+        
+        #button {
+            width: 103%;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    // if (isset($_POST['submit'])) didn't work for me. I need an explanation
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            echo 'Hi '.htmlspecialchars($_POST['name']).'.'.'<br>';
+            echo 'You are '.(int)$_POST['age'].' years old.'.'<br>';
+            echo 'You live in '.htmlspecialchars($_POST['city']).'.';
+        }
+    ?>
+    <form method="post">
+        <input type="text" name="name" placeholder="Name" required><br><br>
+        <input type="text" name="age" placeholder="Age" required><br><br>
+        <input type="text" name="city" placeholder="City" required><br><br>
+        <input type="submit" name="Submit" id="button" value="SUBMIT">
+    </form>
+    
+</body>
+
+</html>
