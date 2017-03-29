ebd15e266b137ce934d2487a2049526d409bbad7
Iveren Favour Shaguy <iverenshaguy@gmail.com>
2017-03-29 12:57:14 +0100

    Update assign1.php
    I concatenated each value of the modulus of $dec to $binary while it is greater than 1. $dec /= 2, divides each answer by 2 and re-finds the modulus.

--- assign1.php
+++ assign1.php
@@ -1,23 +1,49 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Convert Numbers to Binary</title>
+<title>Convert Decimal to Binary</title>
+<style>
+        form {
+            width: 300px;
+            margin: auto;
+            padding-top: 100px;
+        }
+        
+        input {
+            padding: 8px 3px;
+            width: 100%;
+        }
+        
+        #button {
+            width: 103%;
+        }
+</style>
 </head>
 
 <body>
-
-<?php
-//We simply use the decbin() function.
-
-function dectobin($num) {
-  echo decbin($num);
-}
-
-dectobin(1673);
-
+<?php 
+    function decimal_binary($dec){
+        $binary;
+        while($dec >= 1){
+            $rem = $dec % 2;
+            $dec /= 2;
+            $binary = $rem.$binary;
+        }
+        if($binary == null){
+            $binary = 0;
+        }
+        return $binary;
+    }
 ?>
 
+<form method="post">
+    <input type="number" name="num" placeholder="Enter the number" required><br><br>
+    <input value="<?php $dec = 0;
+        if(isset($_POST['submit'])) {
+            $dec = $_POST['num'];
+            echo decimal_binary($dec);}?>" readonly><br><br>
+    <input type="submit" name="submit" id="button" value="SUBMIT">
+</form>
+
 </body>
-
-
 </html>
